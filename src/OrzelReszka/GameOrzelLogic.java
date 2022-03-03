2328a11c1c3dbcdc631fd4c92bd50e9f94207efb
Snusz <artur.snusz@atos.net>
2022-03-03 11:48:43 +0100

    test 2

--- src/OrzelReszka/GameOrzelLogic.java
+++ src/OrzelReszka/GameOrzelLogic.java
@@ -4,7 +4,7 @@ public class GameOrzelLogic {
     int min = 0;
     int max = 1;
 
-
+    //comment added
     public int getRandom(){
         return (int)(Math.random()*(this.max-this.min+1)+this.min);
     }
